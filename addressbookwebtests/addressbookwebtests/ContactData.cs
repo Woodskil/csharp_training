e03e83d1a46739705190d67eea777951d3d5e5ba
Woodskil <Russian-93@mail.ru>
2018-10-10 01:23:43 +0300

    Add ContactCreationTests Add ContactCreationTest Add ContactData

---
+++ addressbookwebtests/addressbookwebtests/ContactData.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string middlename;
+
+        private string lastname = "";
+        private string nickname = "";
+        //private string title = "";
+        //private string company;
+        //private string address;
+        //private string home;
+        //private string mobile;
+        //private string work;
+        //private string fax;
+        //private string email;
+        //private string email2;
+        //private string email3;
+        //private string homepage;
+        //private string bday;
+        //private string bmonth;
+        //private string byear;
+        //private string aday;
+        //private string amonth;
+        //private string ayear;
+        //private GroupData new_group;
+        //private string address2;
+        //private string phone2;
+        //private string notes;
+
+        public ContactData(string firstname, string middlename)
+        {
+            this.firstname = firstname;
+            this.middlename = middlename;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+
+            set
+            {
+                nickname = value;
+            }
+        }
+    }
+}
